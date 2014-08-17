029698b5830dcd915ec820aa6519329b281d3971
Matthew Karas <matthew.james.karas@gmail.com>
2014-08-17 11:29:19 -0400

    Added addTwoNumbers function

--- myModule.cpp
+++ myModule.cpp
@@ -6,9 +6,15 @@ std::string helloWorld()
   return "Hello World";
 }
 
+int addTwoNumbers(int a, int b)
+{
+  return a + b;
+}
+
 CHAISCRIPT_MODULE_EXPORT chaiscript::ModulePtr create_chaiscript_module_myModule()
 {
   chaiscript::ModulePtr m(new chaiscript::Module());
   m->add(chaiscript::fun(helloWorld), "helloWorld");
+  m->add(chaiscript::fun(addTwoNumbers), "addTwoNumbers");
   return m;
 }
