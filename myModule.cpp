4575ef68faacd61bedb3773d7ec3b5c9b736345d
Matthew Karas <matthew.james.karas@gmail.com>
2014-08-17 11:25:56 -0400

    Added export of module function

--- myModule.cpp
+++ myModule.cpp
@@ -5,3 +5,10 @@ std::string hello_world()
 {
   return "Hello World";
 }
+
+CHAISCRIPT_MODULE_EXPORT chaiscript::ModulePtr create_chaiscript_module_myModule()
+{
+  chaiscript::ModulePtr m(new chaiscript::Module());
+  m->add(chaiscript::fun(hello_world), "hello_world");
+  return m;
+}
